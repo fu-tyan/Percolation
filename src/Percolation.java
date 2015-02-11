64b11432116537938cd617b6767a64da5572a102
fu-tyan <fu-tyan@rambler.ru>
2015-02-12 00:58:45 +0300

    percolates() changes

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,90 +2,76 @@ public class Percolation {
 
     private int side;
     private int last;
-    QuickFindUF model;
-
-    private int checkArgument(int i, int j) {
-        if(i <= 0 || i > side || j <= 0 || j > side) throw new IndexOutOfBoundsException("Out of bound exception");
-        return (i - 1) * side + j;
-    }
+    private WeightedQuickUnionUF model;
+    private boolean[][] model2d;
+    private boolean percolated;
 
     public Percolation(int N) {
-        if(N <= 0) throw new IllegalArgumentException("Illegal argument exception");
+        percolated = false;
+        if (N <= 0) throw new IllegalArgumentException("Illegal argument exception");
         side = N;
         last = N * N + 1;
-        model = new QuickFindUF(last + 1);
+        model = new WeightedQuickUnionUF(last + 1);
+        model2d = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                model2d[i][j] = false;
+            }
+        }
+    }
+
+    private int checkArgument(int i, int j) {
+        if (i <= 0 || i > side || j <= 0 || j > side) throw new IndexOutOfBoundsException("Out of bound exception");
+        return (i - 1) * side + j;
     }
 
     public void open(int i, int j) {
         int index = checkArgument(i, j);
-        if(i == 1) {
+        if (i == 1) {
             model.union(index, 0);
         }
-        if(i == side) {
-            model.union(last, index);
-        }
-        if(i > 1 && isOpen(i - 1, j)) {
+        if (i > 1 && isOpen(i - 1, j)) {
             model.union(index, index - side);
         }
-        if(i < side && isOpen(i + 1, j)) {
+        if (i < side && isOpen(i + 1, j)) {
             model.union(index + side, index);
         }
-        if(j > 1 && isOpen(i, j - 1)) {
-            if(model.find(index) > model.find(index - 1)) {
+        if (j > 1 && isOpen(i, j - 1)) {
+            if (model.find(index) > model.find(index - 1)) {
                 model.union(index, index - 1);
             } else {
                 model.union(index - 1, index);
             }
         }
-        if(j < side && isOpen(i, j + 1)) {
-            if(model.find(index) > model.find(index + 1)) {
+        if (j < side && isOpen(i, j + 1)) {
+            if (model.find(index) > model.find(index + 1)) {
                 model.union(index, index + 1);
             } else {
                 model.union(index + 1, index);
             }
         }
-        if (!isOpen(i, j)) {
-            model.union(index, ???);
+        if (i == side) {
+            if (model.connected(0, index)) {
+               percolated = true;
+            }
         }
+        model2d[i - 1][j - 1] = true;
     }
 
     public boolean isOpen(int i, int j) {
-        int index = checkArgument(i, j);
-        if(model.find(index) == index) {
-            return false;
-        }
-        return true;
+        checkArgument(i, j);
+        return model2d[i - 1][j - 1];
     }
 
     public boolean isFull(int i, int j) {
         int index = checkArgument(i, j);
-        if(model.find(index) == 0) {
+        if (model.connected(index, 0)) {
             return true;
         }
         return false;
     }
 
     public boolean percolates() {
-        if(model.find(last) == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public int checkCell(int i, int j) {
-        return model.find(checkArgument(i,j));
-    }
-
-    public static void main(String[] args) {
-        int N = StdIn.readInt();
-        Percolation sis = new Percolation(N);
-        while (!sis.percolates()) {
-            int i = StdIn.readInt();
-            int j = StdIn.readInt();
-            if (sis.isOpen(i, j)) continue;
-            sis.open(i, j);
-            StdOut.println(sis.checkCell(i, j));
-        }
-        StdOut.println("Bingo");
+        return percolated;
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,43 +1,42 @@
 public class PercolationStats {
 
-    private int[] times;
+    private double[] times;
 
     public PercolationStats(int N, int T) {
-        if(N <= 0 || T <= 0) throw new IllegalArgumentException("Argumetn exception");
-        times = new int[T];
-        for(int k = 0; k < T; k++) {
-            StdOut.println("try = " + k);
+        if (N <= 0 || T <= 0) throw new IllegalArgumentException("Argumetn exception");
+        times = new double[T];
+        for (int k = 0; k < T; k++) {
             Percolation model = new Percolation(N);
             int count = 0;
             int i;
             int j;
-            while(!model.percolates()) {
+            while (!model.percolates()) {
                 i = StdRandom.uniform(1, N + 1);
                 j = StdRandom.uniform(1, N + 1);
-                if(!model.isOpen(i, j)) {
+                if (!model.isOpen(i, j)) {
                     model.open(i, j);
                     count++;
                 }
             }
-            times[k] = count;
+            times[k] = (double) count / (N * N);
         }
     }
 
     public double mean() {
-        int sum = 0;
-        for(int k = 0; k < times.length; k++) {
+        double sum = 0;
+        for (int k = 0; k < times.length; k++) {
             sum = sum + times[k];
         }
         return sum / times.length;
     }
     public double stddev() {
-        if(times.length == 1) {
+        if (times.length == 1) {
             return Double.NaN;
         }
         double mean = mean();
         double sum = 0;
-        for(int k = 0; k < times.length; k++) {
-            sum = sum + Math.pow((times[k] - mean), 2);
+        for (int k = 0; k < times.length; k++) {
+            sum = sum + ((times[k] - mean) * (times[k] - mean));
         }
         return Math.sqrt(sum / (times.length - 1));
     }
@@ -49,8 +48,8 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
-        int N = StdIn.readInt();
-        int T = StdIn.readInt();
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
         PercolationStats sis = new PercolationStats(N, T);
         StdOut.println("mean = " + sis.mean());
         StdOut.println("stddev = " + sis.stddev());
